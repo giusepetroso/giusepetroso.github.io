1a50bec81d5abd35ec9860f14efed595963fb8b6
Giuse Petroso <g.petroso@gmail.com>
2022-10-12 16:26:34 +0200

    pino

--- sw.js
+++ sw.js
@@ -15,27 +15,23 @@ self.addEventListener("install", (e) => {
 
 // Fetching content using Service Worker
 self.addEventListener("fetch", (e) => {
-  console.log('fetch');
   e.respondWith(
     (async () => {
-      const response = await fetch(e.request);
-      console.log('pino');
-      // return response;
-      // try {
-      //   console.log(
-      //     `[Service Worker] Fetching resource from web: ${e.request.url}`
-      //   );
-      //   const response = await fetch(e.request);
-      //   console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      //   const cache = await caches.open(cacheName);
-      //   cache.put(e.request, response.clone());
-      //   return response;
-      // } catch (error) {
-      //   console.log(
-      //     `[Service Worker] Fetching resource from cache: ${e.request.url}`
-      //   );
-      //   return await caches.match(e.request);
-      // }
+            try {
+        console.log(
+          `[Service Worker] Fetching resource from web: ${e.request.url}`
+        );
+        const response = await fetch(e.request);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        const cache = await caches.open(cacheName);
+        cache.put(e.request, response.clone());
+        return response;
+      } catch (error) {
+        console.log(
+          `[Service Worker] Fetching resource from cache: ${e.request.url}`
+        );
+        // return await caches.match(e.request);
+      }
     })()
   );
 });
