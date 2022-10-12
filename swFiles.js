2785f466e2bef5e683f27fde50b592a322601abe
Giuse Petroso <g.petroso@gmail.com>
2022-10-12 15:16:08 +0200

    wip

--- swFiles.js
+++ swFiles.js
@@ -1,1 +1,1 @@
-const filesList = ['/page-1.html','/page-2.html','/home.html','/images/whatsapp-image-2022-03-21-at-16.46.53.jpeg'];
+const filesList = ["/page-1.html", "/page-2.html", "/home.html"];
