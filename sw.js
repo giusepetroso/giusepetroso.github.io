0e508274398a4e80b6474045848e2da86491b5df
Giuse Petroso <g.petroso@gmail.com>
2022-10-12 16:05:41 +0200

    wip

--- sw.js
+++ sw.js
@@ -17,21 +17,21 @@ self.addEventListener("install", (e) => {
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
-      try {
-        console.log(
-          `[Service Worker] Fetching resource from web: ${e.request.url}`
-        );
-        const response = await fetch(e.request);
-        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-        const cache = await caches.open(cacheName);
-        cache.put(e.request, response.clone());
-        return response;
-      } catch (error) {
-        console.log(
-          `[Service Worker] Fetching resource from cache: ${e.request.url}`
-        );
-        return await caches.match(e.request);
-      }
+      // try {
+      //   console.log(
+      //     `[Service Worker] Fetching resource from web: ${e.request.url}`
+      //   );
+      //   const response = await fetch(e.request);
+      //   console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      //   const cache = await caches.open(cacheName);
+      //   cache.put(e.request, response.clone());
+      //   return response;
+      // } catch (error) {
+      //   console.log(
+      //     `[Service Worker] Fetching resource from cache: ${e.request.url}`
+      //   );
+      //   return await caches.match(e.request);
+      // }
     })()
   );
 });
